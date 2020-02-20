2d8e4c2221970a9ef1e4ccd77cf09ac96343fa25
Roby Ibrahim Renata <roby.renata@codify.id>
2020-02-20 17:52:23 +0700

    add more button function

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,85 +1,103 @@
-import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
-import { Plugins } from '@capacitor/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from "@angular/core";
+import { Plugins } from "@capacitor/core";
 const { FujitsuThermalPrinter } = Plugins;
 const { Toast } = Plugins;
 
 @Component({
-	selector: 'app-home',
-	templateUrl: 'home.page.html',
-	styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"]
 })
 export class HomePage implements AfterViewInit {
-	scanResult: string = null;
-	@ViewChild('inputRef', { static: false }) scanInput;
+  scanResult: string = null;
+  @ViewChild("inputRef", { static: false }) scanInput;
 
-	constructor() {}
+  constructor() {}
 
-	ngAfterViewInit() {
-		console.log('el ref input scan', this.scanInput);
-		// if ((this.scanInput.nativeElement.value.length = 13)) {
-		// }
-	}
+  ngAfterViewInit() {
+    console.log("el ref input scan", this.scanInput);
+    // if ((this.scanInput.nativeElement.value.length = 13)) {
+    // }
+  }
 
-	ionViewDidEnter() {
-		console.log('scan result?', this.scanResult);
-		this.scanInput.setFocus();
-	}
+  ionViewDidEnter() {
+    console.log("scan result?", this.scanResult);
+    this.scanInput.setFocus();
+  }
 
-	inputChange(event) {
-		console.log('event', event);
-		console.log('event which?', event.which);
-		console.log('ngmodel', this.scanResult);
+  inputChange(event) {
+    console.log("event", event);
+    console.log("event which?", event.which);
+    console.log("ngmodel", this.scanResult);
 
-		console.log('cek keypress?', this.scanInput);
+    console.log("cek keypress?", this.scanInput);
 
-		if (event.which === 13 && this.scanResult.length === 13) {
-			console.log('PRINT');
-			this.scanResult = null;
-			this.allInOnePrinting(this.scanResult);
-		}
-	}
+    if (event.which === 13 && this.scanResult.length === 13) {
+      console.log("PRINT");
+      this.scanResult = null;
+      this.allInOnePrinting(this.scanResult);
+    }
+  }
 
-	allInOnePrinting(textToBePrinted: string) {
-		FujitsuThermalPrinter.GetUsbDevice().then(
-			(res) => {
-				console.log('res permission', res);
-				this.showToast(res.message);
-				if (res) {
-					FujitsuThermalPrinter.Connect().then(
-						(res) => {
-							console.log('res connect', res);
-							this.showToast(res.message);
-							if (res) {
-								FujitsuThermalPrinter.PrintText({ code: textToBePrinted }).then(
-									(res) => {
-										this.showToast(res.message);
-										console.log('res print', res);
-									},
-									(err) => {
-										this.showToast(err.message);
-										console.log('err print', err);
-									},
-								);
-							}
-						},
-						(err) => {
-							this.showToast(err.message);
-							console.log('err connect', err);
-						},
-					);
-				}
-			},
-			(err) => {
-				this.showToast(err.message);
-				console.log('err message', err.message);
-				console.log('err get usb device', err);
-			},
-		);
-	}
+  allInOnePrinting(textToBePrinted: string) {
+    FujitsuThermalPrinter.GetUsbDevice().then(
+      res => {
+        console.log("res permission", res);
+        this.showToast(res.message);
+        if (res) {
+          FujitsuThermalPrinter.Connect().then(
+            res => {
+              console.log("res connect", res);
+              this.showToast(res.message);
+              if (res) {
+                FujitsuThermalPrinter.PrintText({ code: textToBePrinted }).then(
+                  res => {
+                    this.showToast(res.message);
+                    console.log("res print", res);
+                  },
+                  err => {
+                    this.showToast(err.message);
+                    console.log("err print", err);
+                  }
+                );
+              }
+            },
+            err => {
+              this.showToast(err.message);
+              console.log("err connect", err);
+            }
+          );
+        }
+      },
+      err => {
+        this.showToast(err.message);
+        console.log("err message", err.message);
+        console.log("err get usb device", err);
+      }
+    );
+  }
 
-	async showToast(message: string) {
-		await Toast.show({
-			text: message,
-		});
-	}
+  permissionPrint() {
+    FujitsuThermalPrinter.GetUsbDevice().then(res => {
+      this.showToast(res.message);
+    });
+  }
+
+  connectPrint() {
+    FujitsuThermalPrinter.Connect().then(res => {
+      this.showToast(res.message);
+    });
+  }
+
+  print() {
+    FujitsuThermalPrinter.PrintText({
+      code: this.scanResult.toString()
+    }).then(res => this.showToast(res.message));
+  }
+
+  async showToast(message: string) {
+    await Toast.show({
+      text: message
+    });
+  }
 }
